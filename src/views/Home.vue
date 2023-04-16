c0023bde2fc0d2b236cb3a90186adbb99f3de20a
rosmis <rosmis123@gmail.com>
2023-04-16 17:14:45 +0200

    added lottie animation support for randomize btn

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,11 @@
         />
         <HomeGrid v-if="artworks" :grid-array="shuffle(artworks.data.data)" />
         <HomeFooter />
+
+        <div
+            ref="container"
+            class="bg-white rounded-full cursor-pointer shadow-xl p-2 right-4 bottom-4 w-20 z-30 fixed"
+        ></div>
         <!-- <HomeArtWork :artwork="artwork?.data.data" /> -->
     </ui-level>
 </template>
@@ -13,8 +18,24 @@
 <script lang="ts" setup>
 import axios from "axios";
 import { shuffle } from "lodash";
+import lottie from "lottie-web";
+import { onMounted, ref } from "vue";
 import { useQuery } from "vue-query";
+import animationData from "../assets/bouton randomize lottie.json";
 import { headerOptions } from "../composables/useHeadersToken";
+
+const container = ref<HTMLDivElement>();
+
+onMounted(() => {
+    const options = {
+        container: container.value,
+        renderer: "svg",
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+    };
+    lottie.loadAnimation(options);
+});
 
 // const route = useRoute();
 
