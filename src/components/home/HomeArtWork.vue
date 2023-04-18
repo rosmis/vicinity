ef2ddddf0835d9054599c25696776764df5a4e2d
rosmis <rosmis123@gmail.com>
2023-04-18 13:03:36 +0200

    finally fixed blur effect on child except for the hovered image

--- src/components/home/HomeArtWork.vue
+++ src/components/home/HomeArtWork.vue
@@ -5,9 +5,8 @@
         <div
             screen="art"
             class="h-full bg-red-400 rounded-t-2xl w-full top-8 right-0 bottom-0 left-0 absolute"
-            :class="{ filterBlur: isImageHovered }"
         >
-            <ui-level align="right" class="w-full pt-4 pr-4">
+            <ui-level align="right" class="w-full pt-4 pr-4 z-99">
                 <n-icon class="cursor-pointer" @click="emit('close')"
                     ><Close
                 /></n-icon>
@@ -37,16 +36,20 @@
                     </ui-level>
                     <div
                         ref="imageColumn"
-                        class="flex h-full bg-green-500 w-1/2 items-end"
+                        class="flex h-full bg-green-500 w-1/2 items-center justify-center"
                     >
+                        <Transition v-if="isImageHovered" name="fade">
+                            <div class="inset-0 absolute filterBlur"></div>
+                        </Transition>
                         <div
                             v-if="artwork"
                             :style="{
                                 backgroundImage: `url(${artwork.attributes?.mainImage.data.attributes?.formats.medium.url})`,
                                 height: `${formattedImageHeight}px`,
                             }"
-                            class="bg-cover bg-no-repeat hoverEffect"
-                            :class="{ filterUnBlur: isImageHovered }"
+                            class="bg-cover bg-no-repeat cursor-pointer z-10 hoverEffect"
+                            @mouseover="isImageHovered = true"
+                            @mouseleave="isImageHovered = false"
                         ></div>
                     </div>
                 </ui-level>
@@ -68,12 +71,12 @@ const emit = defineEmits<{
 }>();
 
 onMounted(() => {
-    imageColumnWidth.value = imageColumn.value!.clientWidth;
+    imageColumnWidth.value = imageColumn.value!.clientWidth * 0.8;
 });
 
 const imageColumn = ref<HTMLDivElement>();
 const imageColumnWidth = ref<number>();
-const isImageHovered = ref(true);
+const isImageHovered = ref(false);
 
 const formattedImageHeight = computed(() => {
     const artWorkImage =
@@ -87,23 +90,26 @@ const formattedImageHeight = computed(() => {
 
 <style scoped>
 .hoverEffect {
-    position: relative;
-    width: 100%;
+    width: 80%;
     transform-origin: center center;
     transition: all 0.2s ease-in-out;
-    transform: translateZ(0);
 }
-.filterBlur > * {
-    filter: blur(10px);
+
+.filterBlur {
+    backdrop-filter: blur(10px);
 }
-.filterUnBlur {
-    /* mask-image: none;
-    -webkit-mask-image: none;
-    -moz-mask-image: none;
-    -o-mask-image: none;
-    -ms-mask-image: none; */
-}
+
 .hoverEffect:hover {
-    transform: scale(1.3) translateZ(0);
+    transform: scale(1.3);
+}
+
+.fade-enter,
+.fade-leave-to {
+    @apply opacity-0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+    @apply transition-opacity duration-150;
 }
 </style>
