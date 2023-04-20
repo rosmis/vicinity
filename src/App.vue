57835ff162f4030f899ac243ca66ace2f3b15054
rosmis <rosmis123@gmail.com>
2023-04-20 16:05:07 +0200

    added tracking analytics

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <Navbar />
-  <router-view></router-view>
+    <Navbar />
+    <router-view></router-view>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import splitbee from "@splitbee/web";
+
+// This initiliazes Splitbee.js
+splitbee.init();
+</script>
